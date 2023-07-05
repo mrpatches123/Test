ff321f92525bc9aac67d3e3abd3176d2fe11fd93
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2023-07-05 12:57:16 -0400

    prop sizes test

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
-import { world, Player } from '@minecraft/server';
+import { world, Player, DynamicPropertiesDefinition, MinecraftEntityTypes} from '@minecraft/server';
 
-world.afterEvents.itemUse.subscribe((event) => {
-    const { source } = event;
+world.afterEvents.worldInitialize.subscribe((event) => {
+    const {  propertyRegistry } = event;
+    const dynamicPropertiesDefinition = new DynamicPropertiesDefinition();
+	let i = 0;
+	for (;i < 92;i++) {
+	dynamicPropertiesDefinition.defineNumber(`bool${i}`);
+	}
+		try {
+		propertyRegistry.registerEntityTypeDynamicProperties(dynamicPropertiesDefinition, MinecraftEntityTypes.player);
+			
+		} catch (error){
+			console.warn(1000/(i-1),error, error.stack);
+			
+		}
 
 });
-/**
- * @param {Player} player 
- */
-function testForm(player) {
-
-}
